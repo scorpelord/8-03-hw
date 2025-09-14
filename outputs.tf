ef032cd2806d2bdd8f4213f40cea934429f69a8f
scorpelord <mr.molostvov@gmail.com>
2025-09-14 22:51:00 +0300

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -15,20 +15,3 @@ output "vm_db_info" {
     fqdn          = yandex_compute_instance.develop_db.fqdn
   }
 }
-
-# Альтернативный вариант - объединенный вывод
-output "all_vms_info" {
-  description = "Information about all VMs"
-  value = {
-    web_vm = {
-      instance_name = yandex_compute_instance.develop_vm.name
-      external_ip   = yandex_compute_instance.develop_vm.network_interface[0].nat_ip_address
-      fqdn          = yandex_compute_instance.develop_vm.fqdn
-    }
-    db_vm = {
-      instance_name = yandex_compute_instance.develop_db.name
-      external_ip   = yandex_compute_instance.develop_db.network_interface[0].nat_ip_address
-      fqdn          = yandex_compute_instance.develop_db.fqdn
-    }
-  }
-}
