dac8023c7d3079fa26bfdb44bb80935ed809217b
scorpelord <mr.molostvov@gmail.com>
2025-09-14 21:54:15 +0300

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,48 @@
+resource "yandex_vpc_network" "develop" {
+  name = var.vpc_name
+}
+
+resource "yandex_vpc_subnet" "develop_a" {
+  name           = "develop-ru-central-a"
+  zone           = var.default_zone
+  network_id     = yandex_vpc_network.develop.id
+  v4_cidr_blocks = var.default_cidr
+}
+
+data "yandex_compute_image" "ubuntu" {
+  family = "ubuntu-2004-lts"
+}
+
+resource "yandex_compute_instance" "platform" {
+  name        = "netology-develop-platform-web"
+  platform_id = "standard-v4"
+  zone        = var.default_zone
+
+  resources {
+    cores         = 1
+    memory        = 1
+    core_fraction = 5
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu.image_id
+      type     = "network-hdd"
+      size     = 5
+    }
+  }
+
+  scheduling_policy {
+    preemptible = true
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.develop_a.id
+    nat       = true
+  }
+
+  metadata = {
+    serial-port-enable = "1"
+    ssh-keys           = "ubuntu:${var.vms_ssh_root_key}"
+  }
+}
