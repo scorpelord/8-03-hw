14e97fb9339206f42acd5be05cc3925b3d714d01
scorpelord <mr.molostvov@gmail.com>
2025-09-16 16:13:53 +0300

    Create for_each-vm.tf

---
+++ for_each-vm.tf
@@ -0,0 +1,39 @@
+locals {
+  db_vms = {
+    for idx, vm in var.each_vm : vm.vm_name => vm
+  }
+}
+
+resource "yandex_compute_instance" "db" {
+  for_each = local.db_vms
+
+  name        = each.key
+  platform_id = "standard-v3"
+  zone        = var.default_zone
+
+  resources {
+    cores  = each.value.cpu
+    memory = each.value.ram
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd8vmcue7aajpmeo39kk" # Ubuntu 22.04 LTS
+      size     = each.value.disk_volume
+    }
+  }
+
+  network_interface {
+    subnet_id = data.yandex_vpc_subnet.develop.id
+    nat       = true
+    security_group_ids = [yandex_vpc_security_group.db_sg.id]
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${local.ssh_public_key}"
+  }
+
+  scheduling_policy {
+    preemptible = true
+  }
+}
