8eefe376dbc615bfe122e9cce46cc68b73ca7639
scorpelord <mr.molostvov@gmail.com>
2025-09-23 14:55:18 +0300

    Delete ansible.tf

--- ansible.tf
+++
@@ -1,8 +0,0 @@
-resource "local_file" "ansible_inventory" {
-  content = templatefile("inventory.tpl", {
-    webservers = yandex_compute_instance.web[*]
-    databases  = values(yandex_compute_instance.db)[*]
-    storage    = [yandex_compute_instance.storage]
-  })
-  filename = "inventory.ini"
-}
