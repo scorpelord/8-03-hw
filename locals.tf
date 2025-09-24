e54b671ef882f95ca32456569a687efac0b683c0
scorpelord <mr.molostvov@gmail.com>
2025-09-24 16:16:26 +0300

    Create locals.tf

---
+++ locals.tf
@@ -0,0 +1,18 @@
+locals {
+  ssh_public_key = file("~/.ssh/id_rsa.pub")
+}
+#db_vms = {
+ #   "db-vm-1" = {
+  #    name   = "netology-develop-platform-db-1"
+   #   cpu    = 2    
+    #  ram    = 2    
+     # disk   = 20   
+    #}
+    #"db-vm-2" = {
+     # name   = "netology-develop-platform-db-2" 
+     # cpu    = 4
+     # ram    = 4
+     # disk   = 30
+   # }
+ # }
+#}
